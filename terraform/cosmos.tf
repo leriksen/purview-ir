4d00100d13e12cb1e78fc176783dc6b7a99e6a18
Leif Eriksen <leif.eriksen.au@gmail.com>
2025-07-28 00:58:28 +1000

    clean up and rerun of all sources

--- terraform/cosmos.tf
+++ terraform/cosmos.tf
@@ -1,18 +1,18 @@
-# resource "azurerm_cosmosdb_account" "cosmos" {
-#   location            = azurerm_resource_group.rg.location
-#   name = format("%s%s", "leriksenpurviewcosmos01", random_string.suffix.result)
-#   offer_type          = "Standard"
-#   resource_group_name = azurerm_resource_group.rg.name
-#   identity {
-#     type = "SystemAssigned"
-#   }
-#   geo_location {
-#     failover_priority = 0
-#     location          = "australiasoutheast"
-#   }
-#   consistency_policy {
-#     consistency_level       = "BoundedStaleness"
-#     max_interval_in_seconds = 300
-#     max_staleness_prefix    = 100000
-#   }
-# }
+resource "azurerm_cosmosdb_account" "cosmos" {
+  location            = azurerm_resource_group.rg.location
+  name = format("%s%s", "leriksenpurviewcosmos01", random_string.suffix.result)
+  offer_type          = "Standard"
+  resource_group_name = azurerm_resource_group.rg.name
+  identity {
+    type = "SystemAssigned"
+  }
+  geo_location {
+    failover_priority = 0
+    location          = "australiasoutheast"
+  }
+  consistency_policy {
+    consistency_level       = "BoundedStaleness"
+    max_interval_in_seconds = 300
+    max_staleness_prefix    = 100000
+  }
+}
--- terraform/purview.tf
+++ terraform/purview.tf
@@ -6,94 +6,94 @@ module "purview_account" {
   location            = azurerm_resource_group.rg.location
 }
 
-# module "purview_mvnet" {
-#   source = "./modules/purview-managed-vnet"
-#   depends_on = [
-#     module.purview_account
-#   ]
-#   purview_name       = trimprefix(module.purview_account.scan_endpoint, "https://")
-#   purview_mvnet_name = "custom_mvnet"
-# }
-#
-# resource "time_sleep" "mvnet_wait" {
-#   depends_on = [
-#     module.purview_mvnet
-#   ]
-#   create_duration  = "120s"
-#   # destroy_duration = "s"
-# }
-#
-# module "purview_mir" {
-#   source = "./modules/purview-managed-ir"
-#
-#   depends_on = [
-#     time_sleep.mvnet_wait
-#   ]
-#
-#   purview_endpoint        = local.purview_endpoint
-#   purview_id              = module.purview_account.id
-#   purview_managed_storage = module.purview_account.managed_resources.storage_account_id
-#   kind                    = "Managed"
-#   ir_name                 = "MIR"
-#   mvnet_reference         = module.purview_mvnet.mvnet.name
-# }
-#
-# resource "time_sleep" "mir_wait" {
-#   depends_on = [
-#     module.purview_mir
-#   ]
-#   create_duration = "660s"
-# }
-#
-# module "purview_adls_pe" {
-#   depends_on = [
-#     time_sleep.wait
-#   ]
-#   source           = "./modules/purview-managed-endpoint"
-#   purview_endpoint = local.purview_endpoint
-#   mvnet_name       = module.purview_mvnet.mvnet.name
-#   name             = "adls"
-#   resource_id      = azurerm_storage_account.adls.id
-#   resource_kind    = "sa"
-#   subresource      = "dfs"
-# }
-#
-# # key vaults have a different schema for representing their PE approval requests....
-# module "purview_kv_pe" {
-#   depends_on = [
-#     time_sleep.wait
-#   ]
-#   source           = "./modules/purview-managed-endpoint"
-#   purview_endpoint = local.purview_endpoint
-#   mvnet_name       = module.purview_mvnet.mvnet.name
-#   name             = "kv"
-#   resource_id      = azurerm_key_vault.kv.id
-#   resource_kind    = "kv"
-#   subresource      = "vault"
-# }
-#
-# module "purview_cosmos_pe" {
-#   depends_on = [
-#     time_sleep.wait
-#   ]
-#   source           = "./modules/purview-managed-endpoint"
-#   purview_endpoint = local.purview_endpoint
-#   mvnet_name       = module.purview_mvnet.mvnet.name
-#   name             = "cosmosdb"
-#   resource_id      = azurerm_cosmosdb_account.cosmos.id
-#   resource_kind    = "cosmosdb"
-#   subresource      = "SQL"
-# }
-#
-# module "purview_synapse_pe" {
-#   depends_on = [
-#     time_sleep.wait
-#   ]
-#   source           = "./modules/purview-managed-endpoint"
-#   purview_endpoint = local.purview_endpoint
-#   mvnet_name       = module.purview_mvnet.mvnet.name
-#   name             = "synapse"
-#   resource_id      = azurerm_synapse_workspace.synapse.id
-#   resource_kind    = "synapse"
-#   subresource      = "SqlOnDemand"
-# }
+module "purview_mvnet" {
+  source = "./modules/purview-managed-vnet"
+  depends_on = [
+    module.purview_account
+  ]
+  purview_name       = trimprefix(module.purview_account.scan_endpoint, "https://")
+  purview_mvnet_name = "custom_mvnet"
+}
+
+resource "time_sleep" "mvnet_wait" {
+  depends_on = [
+    module.purview_mvnet
+  ]
+  create_duration  = "120s"
+  # destroy_duration = "s"
+}
+
+module "purview_mir" {
+  source = "./modules/purview-managed-ir"
+
+  depends_on = [
+    time_sleep.mvnet_wait
+  ]
+
+  purview_endpoint        = local.purview_endpoint
+  purview_id              = module.purview_account.id
+  purview_managed_storage = module.purview_account.managed_resources[0].storage_account_id
+  kind                    = "Managed"
+  ir_name                 = "MIR"
+  mvnet_reference         = module.purview_mvnet.mvnet.name
+}
+
+resource "time_sleep" "mir_wait" {
+  depends_on = [
+    module.purview_mir
+  ]
+  create_duration = "660s"
+}
+
+module "purview_adls_pe" {
+  depends_on = [
+    time_sleep.mir_wait
+  ]
+  source           = "./modules/purview-managed-endpoint"
+  purview_endpoint = local.purview_endpoint
+  mvnet_name       = module.purview_mvnet.mvnet.name
+  name             = "adls"
+  resource_id      = azurerm_storage_account.adls.id
+  resource_kind    = "sa"
+  subresource      = "dfs"
+}
+
+# key vaults have a different schema for representing their PE approval requests....
+module "purview_kv_pe" {
+  depends_on = [
+    time_sleep.mir_wait
+  ]
+  source           = "./modules/purview-managed-endpoint"
+  purview_endpoint = local.purview_endpoint
+  mvnet_name       = module.purview_mvnet.mvnet.name
+  name             = "kv"
+  resource_id      = azurerm_key_vault.kv.id
+  resource_kind    = "kv"
+  subresource      = "vault"
+}
+
+module "purview_cosmos_pe" {
+  depends_on = [
+    time_sleep.mir_wait
+  ]
+  source           = "./modules/purview-managed-endpoint"
+  purview_endpoint = local.purview_endpoint
+  mvnet_name       = module.purview_mvnet.mvnet.name
+  name             = "cosmosdb"
+  resource_id      = azurerm_cosmosdb_account.cosmos.id
+  resource_kind    = "cosmosdb"
+  subresource      = "SQL"
+}
+
+module "purview_synapse_pe" {
+  depends_on = [
+    time_sleep.mir_wait
+  ]
+  source           = "./modules/purview-managed-endpoint"
+  purview_endpoint = local.purview_endpoint
+  mvnet_name       = module.purview_mvnet.mvnet.name
+  name             = "synapse"
+  resource_id      = azurerm_synapse_workspace.synapse.id
+  resource_kind    = "synapse"
+  subresource      = "SqlOnDemand"
+}
